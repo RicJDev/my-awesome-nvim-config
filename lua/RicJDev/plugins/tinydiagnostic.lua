41a29137fb59e8c895be2c958f86674c78017b68
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-04-20 15:03:26 -0400

    FEAT: aregando soporte para git

--- lua/RicJDev/plugins/tinydiagnostic.lua
+++ lua/RicJDev/plugins/tinydiagnostic.lua
@@ -5,10 +5,13 @@ return {
   config = function()
     require('tiny-inline-diagnostic').setup({
       options = {
+        preset = 'powerline',
         multilines = {
           enabled = true,
           always_show = true,
         },
+        enable_on_insert = true,
+        throttle = 0,
       }
     })
     vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
