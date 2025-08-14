b1656963136101c3bdee94cb9a39abd779493602
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-08-13 21:38:19 -0400

    Eliminando archivo de configuracion

--- lua/RicJDev/plugins/file-tree.lua
+++ lua/RicJDev/plugins/file-tree.lua
@@ -7,4 +7,17 @@ return {
     "MunifTanjim/nui.nvim",
   },
   lazy = false, 
+
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        hide_by_name = { ".git", "node_modules" },
+      },
+
+      hijack_netrw_behavior = "disabled",
+    },
+  }
 } 
