f49640a718376f7f125467ed440b0bf13a93745f
RicMonster1 <147381654+RicMonster1@users.noreply.github.com>
2025-08-15 00:37:35 -0400

    Formato minimo :D

--- lua/RicJDev/plugins/formatter.lua
+++ lua/RicJDev/plugins/formatter.lua
@@ -4,7 +4,11 @@ return {
     require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua", lsp_format = "fallback" },
-        javascript = { "prettier", stop_after_first = true, lsp_format = "fallback" },
+        javascript = {
+          "prettier",
+          stop_after_first = true,
+          lsp_format = "fallback",
+        },
       },
     }
   end,
